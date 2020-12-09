f9b126018b897e4a963235c51558f14276b1af80
杰 <@JIE>
2020-12-09 16:47:23 +0800

    王鹤接受

---
+++ fiveone/Controllers/WangHeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fiveone.Controllers
+{
+    public class WangHeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
