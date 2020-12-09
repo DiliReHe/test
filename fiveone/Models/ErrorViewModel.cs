fbe3c59f4df7a320cd5386acdf6fd8bc3feceae2
迪丽热鹤 <@DESKTOP-EF2T9J8>
2020-12-09 15:14:32 +0800

    添加项目文件。

---
+++ fiveone/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace fiveone.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
